69bf93076d3d266405b6807b67529ad9803772a2
PANdas <krsy0411@naver.com>
2023-12-16 18:50:43 +0900

    shell 정렬

--- C/Sort/Shell/shell.c
+++ C/Sort/Shell/shell.c
@@ -25,18 +25,20 @@ void randomNumberInsertTo(int array[]) {
     }
 }
 
-// 현재 잘 이해가 안 가는 코드
+// 현재 명확하게는 이해가 안 가는 코드
 void shellSort(int array[]) {
     // 간격 설정해서 순회
     for(int gap = SIZE/2; gap>0; gap /= 2) {
         // 간격 내에서 배열 끝까지 순회하며 정렬 수행
         for(int i=gap; i<SIZE; i++) {
+            // 스왑을 위한 임시값
             int temp = array[i];
             int j;
-
+            // 간격만큼 배열 내부를 이동하면서 삽입정렬 수행 : 앞값이 뒷값보다 큰 경우에만 계속해서 값을 갱신해주기
             for(j=i; j>=gap && array[j-gap] > temp; j -= gap) {
                 array[j] = array[j-gap];
             }
+            // 스왑 작업
             array[j] = temp;
         }
     }
