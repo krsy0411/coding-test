dfc29e0409dceabb0d469dfb834b1fac80423d36
DevNerd <90031820+krsy0411@users.noreply.github.com>
2024-04-12 15:11:33 +0900

    [Silver IV] Title: 스택 2, Time: 1012 ms, Memory: 175804 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.js
@@ -0,0 +1,42 @@
+const [N,...arr] = require("fs").readFileSync("/dev/stdin")
+.toString()
+.trim()
+.split("\n");
+
+const stack = [];
+const results = []; // 1. 배열로 두었을 때
+
+for (let i = 0; i < arr.length; i++) {
+  let query = arr[i];
+  let [cmd, value] = query.split(" ");
+
+  switch (cmd) {
+    case "1":
+      stack.push(parseInt(value));
+      break;
+    case "2":
+      if (stack.length > 0) {
+        results.push(stack.pop());
+      } else {
+        results.push(-1);
+      }
+      break;
+    case "3":
+      results.push(stack.length);
+      break;
+    case "4":
+      results.push(stack.length === 0 ? 1 : 0);
+      break;
+    case "5":
+      if (stack.length > 0) {
+        results.push(stack[stack.length - 1]);
+      } else {
+        results.push(-1);
+      }
+      break;
+    default:
+      break;
+  }
+}
+
+console.log(results.join("\n"));
