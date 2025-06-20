606ebe415f4d864166cecd29774d1ff803b2e5a0
ThisTimeNull <90031820+krsy0411@users.noreply.github.com>
2025-06-20 15:06:58 +0900

    [Silver III] Title: 파도반 수열, Time: 92 ms, Memory: 9604 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.js
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.js
@@ -1,21 +1,21 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const T = Number(input[0]);
-const MAX_N = 100;
+const MAX = 100;
 
-const dp = new Array(MAX_N + 1).fill(0);
 const result = [];
+const dp = new Array(MAX+1).fill(0);
 
-// 초기화
-dp[1]=1; dp[2]=1; dp[3]=1;
-for(let i=4; i<=MAX_N; i++) {
+// 점화식 초기화
+dp[1] = 1;
+dp[2] = 1;
+dp[3] = 1;
+for(let i=4; i<=MAX; i++) {
     dp[i] = dp[i-2] + dp[i-3];
 }
 
-// 테스트 케이스 처리
 for(let t=1; t<=T; t++) {
     const N = Number(input[t]);
-    // 메모이제이션 내용을 기반으로 P(N) 찾아내기
     result.push(dp[N]);
 }
 
