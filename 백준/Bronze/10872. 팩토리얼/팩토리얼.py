f54ffe64c6bb8ada75d88837638183bf434b9eaa
ThisTimeNull <90031820+krsy0411@users.noreply.github.com>
2025-07-12 11:34:20 +0900

    [Bronze III] Title: 팩토리얼, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input().strip())
+def factorial(n):
+    if n == 1 or n == 0:
+        return 1
+    return factorial(n-1) * n
+    
+sys.stdout.write(str(factorial(N)) + '\n')
