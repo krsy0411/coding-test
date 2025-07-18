37670c1aae2f0c9b4a73c6a7fb9236376bbd4e04
ThisTimeNull <90031820+krsy0411@users.noreply.github.com>
2025-07-18 20:26:11 +0900

    [Silver IV] Title: 제로, Time: 116 ms, Memory: 114604 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+K = int(input().strip())
+
+stack = []
+for _ in range(K):
+    num = int(input().strip())
+    
+    if num == 0:
+        stack.pop()
+    else:
+        stack.append(num)
+        
+print(sum(stack))
