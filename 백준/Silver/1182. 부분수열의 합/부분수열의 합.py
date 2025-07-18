65ad4d96d7f68448bdb81ea3f3ac533652b6efbf
ThisTimeNull <90031820+krsy0411@users.noreply.github.com>
2025-07-18 14:32:18 +0900

    [Silver II] Title: 부분수열의 합, Time: 132 ms, Memory: 111276 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,25 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-N, S = map(int, input().strip().split())
+N,S = map(int, input().strip().split())
 arr = list(map(int, input().strip().split()))
-count = 0
 
-def solve_recursive(current_sum, depth):
-    global count
-    
-    if depth == N:
-        if current_sum == S:
-            count += 1
-        return
+def recursive_solve(depth, total):
+	if depth == N:
+		return 1 if total == S else 0
+	
+	return recursive_solve(depth+1, total + arr[depth]) + recursive_solve(depth+1, total)
 
-    solve_recursive(current_sum + arr[depth], depth + 1) # 포함
-    solve_recursive(current_sum, depth + 1) # 포함X
-
-solve_recursive(0,0)
-
-# S가 0인 경우 : 첫 시점에 아무것도 선택하지 않은 경우도 하나의 부분수열로 간주되므로
+result = recursive_solve(0, 0)
 if S == 0:
-    count -= 1
-
-sys.stdout.write(f"{count}\n")
+	result -= 1
+	
+print(result)
