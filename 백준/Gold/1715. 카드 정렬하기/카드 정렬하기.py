9ad8b8a56751faa045816d7ac8a1097165522d92
ThisTimeNull <90031820+krsy0411@users.noreply.github.com>
2025-07-22 21:56:22 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 244 ms, Memory: 113564 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -1,19 +1,20 @@
-# 백준 1655 : 카드 정렬하기
-import heapq
 import sys
+import heapq
 input = sys.stdin.readline
 
 N = int(input().strip())
-pq = [int(input().strip()) for _ in range(N)] 
-heapq.heapify(pq) # 힙 만들기 : 최소힙
+cards = [int(input().strip()) for _ in range(N)]
+heapq.heapify(cards)
+result = 0 # 결과 : 총 누적합
 
-result = 0 # 최소한의 비교횟수
-while len(pq) > 1:
-    num_first = heapq.heappop(pq)
-    num_second = heapq.heappop(pq)
-    temp_sum = num_first + num_second
-    result += temp_sum
-    
-    heapq.heappush(pq, temp_sum)
-    
+# 최소힙에서 2개씩 꺼내서 합산 후 다시 힙에 추가 -> 반복
+while len(cards) >= 2:
+	num_first = heapq.heappop(cards)
+	num_second = heapq.heappop(cards)
+	sum_result = (num_first + num_second)
+	
+	# 힙에 합산 결과를 넣고 결과를 누적
+	heapq.heappush(cards, sum_result)
+	result += sum_result 
+	
 print(result)
