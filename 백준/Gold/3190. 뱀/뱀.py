bae36093129441dd13c946fbbdf287bbfb56d7e9
ThisTimeNull <90031820+krsy0411@users.noreply.github.com>
2025-07-23 12:58:58 +0900

    [Gold IV] Title: 뱀, Time: 112 ms, Memory: 113500 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,4 +1,3 @@
-# 백준 3190 : 뱀
 import sys
 from collections import deque
 input = sys.stdin.readline
@@ -6,52 +5,52 @@ input = sys.stdin.readline
 N = int(input().strip())
 K = int(input().strip())
 
-graph = [[0] * N for _ in range(N)] # N x N 크기의 맵
+graph = [[0] * N for _ in range(N)]
 for _ in range(K):
-    x,y = map(int, input().strip().split())
-    graph[x-1][y-1] = 2 # 2는 사과가 있음을 의미, 1은 뱀의 몸통이나 머리가 있음을 의미
+	row, col = map(int, input().strip().split())
+	graph[row-1][col-1] = 2 # 그래프 상의 2는 사과가 있음을 의미, 1은 뱀\
 
 L = int(input().strip())
-turns = {} # 딕셔너리
+directions = {}
 for _ in range(L):
-    num, direction = input().strip().split()
-    turns[int(num)] = direction
-    
-second = 0 # 몇 초 걸렸는지 확인(결과)
-queue = deque([(0,0)]) # 큐 생성 : 꼬리와 머리 좌표 보관용
-graph[0][0] = 1
+	t, d = input().strip().split()
+	directions[int(t)] = d
 
-is_end = True # 게임 종료 알림용
-move = [(0,1), (1,0), (0,-1), (-1,0)] # 시계방향
-move_idx = 0 # 움직이는 방향에 대한 인덱스
-while is_end:
-    second += 1
-    
-    x, y = queue[-1] # 머리 현재 좌표
-    dx, dy = move[move_idx]
-    nx, ny = x + dx, y + dy
-    # 충돌 확인 & 사과
-    if 0 <= nx < N and 0 <= ny < N and graph[nx][ny] != 1:
-        # 사과가 아닌 경우에 : 꼬리는 제거
-        if graph[nx][ny] != 2:
-            tail_x, tail_y = queue.popleft()
-            graph[tail_x][tail_y] = 0
-        
-        # 사과가 있든 없든 머리는 이동
-        graph[nx][ny] = 1
-        queue.append((nx,ny))
-    else:
-        # 벽이나 몸통에 충돌한 경우에는 게임 종료
-        break
-    
-    # 방향전환
-    if second in turns:
-        if turns[second] == 'D':
-            # 오른쪽 90도 회전
-            move_idx = (move_idx + 1) % 4
-        else:
-            # 왼쪽 90도 회전
-            move_idx = (move_idx + 3) % 4
-    
+# 주요 로직 시작
+queue = deque([(0,0)])
+time = 0 # 결과
+
+# 이동방향 : 동남서북
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+move_idx = 0
+
+while True:
+	# 시간 증가
+	time += 1
+	
+	# 현재와 다음 위치 좌표 계산
+	current_row, current_col = queue[-1][0], queue[-1][1]
+	next_row, next_col = queue[-1][0] + dx[move_idx], queue[-1][1] + dy[move_idx]
+	
+	if 0 <= next_row < N and 0 <= next_col < N and graph[next_row][next_col] != 1:
+		# 사과가 존재하지 않는 경우 -> 꼬리도 움직임
+		# 사과가 존재하는 경우 -> 머리만 움직임
+		if graph[next_row][next_col] != 2:
+			tail_row, tail_col = queue.popleft() # 꼬리 꺼내기
+			graph[tail_row][tail_col] = 0
+		
+		queue.append((next_row, next_col)) # 머리 위치는 큐에 삽입
+		graph[next_row][next_col] = 1
+		
+		# 방향 전환할 시간인지 확인하고 적용
+		if time in directions:
+			if directions[time] == 'L':
+				move_idx = (move_idx - 1) % 4
+			else:
+				move_idx = (move_idx + 1) % 4
+	else:
+		break
+
 # 결과 출력
-print(second)
+print(time)
