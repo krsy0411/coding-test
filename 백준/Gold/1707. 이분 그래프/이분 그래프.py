88d03d48cac91a4c229e2c362ea8cddc862ebe16
ThisTimeNull <90031820+krsy0411@users.noreply.github.com>
2025-07-26 21:03:49 +0900

    [Gold IV] Title: 이분 그래프, Time: 596 ms, Memory: 191020 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,38 +1,44 @@
+# 백준 1707 : 이분 그래프(너비우선탐색)
 import sys
-sys.setrecursionlimit(10**6)
+from collections import deque
 input = sys.stdin.readline
 
 T = int(input().strip())
 
-def dfs(graph, node, group):
-    for neighbor_node in graph[node]:
-        if group[neighbor_node] is None:
-            group[neighbor_node] = 1 - group[node]
-            
-            if not dfs(graph, neighbor_node, group):
-                return False
-        elif group[neighbor_node] == group[node]:
-            return False
+def bfs(graph, group, node):
+    queue = deque([node])
+    group[node] = 0
     
-    return True
+    while queue:
+        current_node = queue.popleft()
+        
+        for neighbor_node in graph[current_node]:
+            if group[neighbor_node] is None:
+                group[neighbor_node] = 1 - group[current_node]
+                queue.append(neighbor_node)
+            elif group[neighbor_node] == group[current_node]:
+                return False
 
+    # 이분 그래프인 경우
+    return True
+        
+
+# 테스트 케이스만큼 반복
 for _ in range(T):
-    V, E = map(int, input().strip().split())
-    group = [None] * (V + 1)
+    V,E = map(int, input().strip().split())
     graph = [[] for _ in range(V + 1)]
+    group = [None] * (V + 1)
+    
     for _ in range(E):
         u,v = map(int, input().strip().split())
         graph[u].append(v)
-        graph[v].append(u) # 무방향 그래프
+        graph[v].append(u)
     
     is_bipartite = True
-    for i in range(1, V+1):
-        if group[i] is None:
-            group[i] = 0
-            
-            if not dfs(graph, i, group):
+    for vertex in range(1, V+1):
+        if group[vertex] is None:
+            if not bfs(graph, group, vertex):
                 is_bipartite = False
                 break
     
-    # 결과 출력
     print('YES' if is_bipartite else 'NO')
