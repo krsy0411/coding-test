6e947a96b3db279729a060760d4dcc5ea4af3151
ThisTimeNull <90031820+krsy0411@users.noreply.github.com>
2025-07-26 11:57:20 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1004 ms, Memory: 162076 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,39 +1,35 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 
-#  4개의 도시, 4개의 도로, 거리 정보 2, 출발 도시 번호 1
-n, m, k, x = map(int, input().split())
+N,M,K,X = map(int, input().strip().split())
+visited = [False] * (N+1)
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    A, B = map(int, input().strip().split())
+    graph[A].append(B) # 단방향 도로
 
-# 각 도시들에 대한 정보 : 각 도시마다 어떤 도시로 갈 수 있는지 담는 배열 : (0),1,......,n번 도시
-graph = [[] for _ in range(n+1)]
-# 모든 도시들에 대한 최단거리 정보 : (0), 1,.....,n번 도시
-shortDistance = [-1]*(n+1)
-# 출발 도시에서 출발 도시까지의 거리는 0
-shortDistance[x] = 0
+def bfs(graph, visited, node=X):
+    queue = deque([(node,0)]) # (노드 번호, 거리)
+    visited[node] = True # 시작 노드 방문 처리
+    result = [] # 결과 처리용 배열
 
-for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a].append(b)
+    while queue:
+        current_node, current_dist = queue.popleft()
 
-# x번 도시에서 출발
-q = deque([x])
-while q:
-    currentCity = q.popleft()
-    # 현재 도시 인근의 도시들 탐색
-    for nextCity in graph[currentCity]:
-        # 아직 최단거리 정보가 -1이면 : 즉, 아직 방문하지 않았다면
-        if shortDistance[nextCity] == -1:
-            # 현재까지의 거리 + 1 한게 다음 도시까지의 거리
-            shortDistance[nextCity] = shortDistance[currentCity] + 1
-            q.append(nextCity)
+        if current_dist == K:
+            result.append(current_node)
+        
+        for neighbor_node in graph[current_node]:
+            if not visited[neighbor_node]:
+                visited[neighbor_node] = True
+                queue.append((neighbor_node, current_dist + 1))
 
-isValid = False
-# 1번 도시부터 n번 도시까지
-for i in range(1, n+1):
-    # 거리 정보와 최단거리 값이 동일하면 해당 도시 번호 오름차순 출력
-    if shortDistance[i] == k:
-        print(i)
-        isValid = True
+    return sorted(result)
 
-# 만약 일치하는 최단거리 값이 없다면?
-if (isValid == False):
+result = bfs(graph, visited, X)
+if not result:
     print(-1)
+else:
+    for node in result:
+        print(node)
