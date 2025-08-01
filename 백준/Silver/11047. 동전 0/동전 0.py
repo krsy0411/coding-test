09d35344d82ad4f05398e0152e3d65c4dbd7aa65
ThisTimeNull <90031820+krsy0411@users.noreply.github.com>
2025-08-01 23:13:27 +0900

    [Silver IV] Title: 동전 0, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,18 +1,15 @@
-n, k = map(int, input().split())
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().strip().split())
+coins = [int(input().strip()) for _ in range(n)]
+
 count = 0
-list = []
-for _ in range(n):
-  list.append(int(input()))
-# 최소값을 위해 내림차순 정렬 : O(N)
-list.sort(reverse=True)
-
-# 큰 수부터 차례대로 나눠본다
-for i in range(len(list)):
-  # 몫이 0인 경우
-  if(k // list[i] == 0):
-    continue
-  # 몫이 0이 아닌 경우
-  count += k // list[i]
-  k = k % list[i]
-
+for coin in reversed(coins):
+    if k == 0:
+        break
+    
+    count += k // coin
+    k %= coin
+    
 print(count)
