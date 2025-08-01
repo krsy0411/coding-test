2440de1d3c90cd387a3a6afd83bd764d1d114a33
ThisTimeNull <90031820+krsy0411@users.noreply.github.com>
2025-08-01 14:30:16 +0900

    [Silver III] Title: 01타일, Time: 104 ms, Memory: 117360 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+dp = [0] * 1000001
+dp[1] = 1
+dp[2] = 2
+def solve(n):
+    if n != 1 and n != 2:
+        for i in range(3, n + 1):
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 15746
+    
+    return dp[n]
+
+n = int(input().strip())
+print(solve(n))
