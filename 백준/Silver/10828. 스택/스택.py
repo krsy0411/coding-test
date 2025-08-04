cbe700e7725403604abc64d328a16865d67a9e2d
ThisTimeNull <90031820+krsy0411@users.noreply.github.com>
2025-08-04 11:32:36 +0900

    [Silver IV] Title: 스택, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -3,17 +3,25 @@ input = sys.stdin.readline
 
 N = int(input().strip())
 stack = []
-
 for _ in range(N):
     command = input().strip().split()
     
     if command[0] == 'push':
-        stack.append(int(command[1]))
+        stack.append(command[1])
     elif command[0] == 'pop':
-        print(stack.pop() if stack else -1)
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
     elif command[0] == 'size':
         print(len(stack))
     elif command[0] == 'empty':
-        print(0 if stack else 1)
-    else:
-        print(stack[-1] if len(stack) else -1) 
+        if stack:
+            print(0)
+        else:
+            print(1)
+    elif command[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
