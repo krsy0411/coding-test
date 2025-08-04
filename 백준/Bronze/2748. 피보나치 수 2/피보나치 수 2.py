aa68de926f49ac948edb72cd634e4827efb8c9f3
ThisTimeNull <90031820+krsy0411@users.noreply.github.com>
2025-08-04 13:28:40 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,18 +1,11 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input().strip())
-dp = [0] * (n + 1)
+N = int(input().strip())
+dp = [0] * (N + 1)
 dp[1] = 1
 
-def fibonacci(n):
-    if n == 0:
-        return 0
+for i in range(2, N+1):
+    dp[i] = dp[i-1] + dp[i-2]
     
-    if dp[n] != 0:
-        return dp[n]
-    
-    dp[n] = fibonacci(n - 1) + fibonacci(n - 2)
-    return dp[n]
-
-print(fibonacci(n))
+print(dp[N])
