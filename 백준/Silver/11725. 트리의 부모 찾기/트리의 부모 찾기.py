0490c3b75dba1fc2478e44682a0aff708cca7879
ThisTimeNull <90031820+krsy0411@users.noreply.github.com>
2025-08-20 22:47:01 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 304 ms, Memory: 71720 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,30 +1,27 @@
 import sys
-from collections import deque
+sys.setrecursionlimit(10**7)
 input = sys.stdin.readline
 
-N = int(input().strip())
-parent = [i for i in range(N+1)] # 각 노드의 부모 노드 정보 저장 배열 : 우선 자기 자신으로 초기화
-visited = [False] * (N + 1)
+N = int(input().strip()) # 노드 개수
+parent = [0] * (N+1)
 graph = [[] for _ in range(N+1)]
+visited = [False] * (N+1)
+
+# 간선(연결관계) 설정
 for _ in range(N-1):
     u,v = map(int, input().strip().split())
     graph[u].append(v)
     graph[v].append(u)
-
-def bfs(graph, visited, parent, node):
-    queue = deque([node])
+    
+# DFS 수행
+def recursive_search(graph, parent, visited, node):
     visited[node] = True
     
-    while queue:
-        current_node = queue.popleft()
-        
-        for neighbor_node in graph[current_node]:
-            if not visited[neighbor_node]:
-                # 방문처리하고 부모 노드로 설정
-                visited[neighbor_node] = True
-                parent[neighbor_node] = current_node
-                queue.append(neighbor_node)
-                
-bfs(graph, visited, parent, 1)
-for i in range(2, N+1):
-    print(parent[i])
+    for next_node in graph[node]:
+        if not visited[next_node]:
+            parent[next_node] = node
+            recursive_search(graph, parent, visited, next_node)
+            
+# 결과 출력
+recursive_search(graph, parent, visited, 1)
+sys.stdout.write('\n'.join(map(str, parent[2:])) + '\n')
