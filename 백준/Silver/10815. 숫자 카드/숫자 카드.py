99d9b52403ea32a386d106e43ad0c222e5068f88
ThisTimeNull <90031820+krsy0411@users.noreply.github.com>
2025-09-09 10:58:43 +0900

    [Silver V] Title: 숫자 카드, Time: 788 ms, Memory: 110864 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,17 +1,22 @@
-# 백준 10815 : 숫자 카드
 import sys
+import bisect
 input = sys.stdin.readline
 
-n = int(input().strip())
-cards = set(map(int, input().strip().split()))
+N = int(input().strip())
+sangeun_nums = list(map(int, input().strip().split()))
+sangeun_nums.sort()
 
-m = int(input().strip())
+M = int(input().strip())
+target_nums = list(map(int, input().strip().split()))
+
 result = []
-nums = list(map(int, input().strip().split()))
-for num in nums:
-    if num in cards:
+for target in target_nums:
+    idx = bisect.bisect_left(sangeun_nums, target)
+
+    # 이진 탐색을 통해 타겟이 존재하는지 확인
+    if (idx < len(sangeun_nums) and (sangeun_nums[idx] == target)):
         result.append(1)
     else:
         result.append(0)
-        
-print(' '.join(map(str, result)))
+
+sys.stdout.write('\n'.join(map(str, result)))
