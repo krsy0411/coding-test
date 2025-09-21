7e1965e15d32212588a4def394de2250e3e8fba7
ThisTimeNull <90031820+krsy0411@users.noreply.github.com>
2025-09-21 15:10:09 +0900

    [Silver IV] Title: 귀찮아 (SIB), Time: 80 ms, Memory: 42660 KB -BaekjoonHub

--- 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.py
+++ 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.py
@@ -2,18 +2,13 @@ import sys
 input = sys.stdin.readline
 
 n = int(input().strip())
-arr = list(map(int, input().strip().split()))
+x = list(map(int, input().strip().split()))
 
-# 누적합 배열
-prefix_sum = [0] * (n + 1)
+answer = 0
+sum_x = sum(x)
 
-# 누적합 계산
-for i in range(1, n + 1):
-    prefix_sum[i] = prefix_sum[i - 1] + arr[i - 1]
-
-result = 0
-for i in range(n):
-    # arr[i]와 그 뒤의 모든 원소들의 합을 곱함
-    result += (arr[i] * (prefix_sum[n] - prefix_sum[i + 1]))
-
-print(result)
+for xi in x:
+    sum_x -= xi
+    answer += (xi * sum_x)
+    
+print(answer)
