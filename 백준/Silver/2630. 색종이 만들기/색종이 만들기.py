43c9b4c1345b8de26ff2812bbdfa8fb6c42cc012
ThisTimeNull <90031820+krsy0411@users.noreply.github.com>
2025-09-27 23:09:25 +0900

    [Silver II] Title: 색종이 만들기, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,26 +1,42 @@
 import sys
+sys.setrecursionlimit(10**6)
+
 input = sys.stdin.readline
+N = int(input().strip()) # 2^k (1 <= k <= 7)
+paper = [list(map(int, input().strip().split())) for _ in range(N)] # 0 : 하얀색, 1 : 파란색
 
-N = int(input().strip())
-paper = [list(map(int, input().strip().split())) for _ in range(N)]
+# 잘린 종이의 개수를 세는 변수 : 하얀색 종이, 파란색 종이
+white_count = 0
+blue_count = 0
 
-def recursive_count(x, y, size, counts):
-    color = paper[x][y] # 현재 색종이의 색
+def calculate_counts(x, y, size):
+    global white_count, blue_count
     
+    # 현재 사이즈에서 모든 색이 동일한지 확인
+    initial_color = paper[x][y]
+    all_same = True
     for i in range(x, x + size):
         for j in range(y, y + size):
-            if paper[i][j] != color:
-                recursive_count(x, y, size // 2, counts)  # 왼쪽 위
-                recursive_count(x, y + size // 2, size // 2, counts) # 오른쪽 위
-                recursive_count(x + size // 2, y, size // 2, counts) # 왼쪽 아래
-                recursive_count(x + size // 2, y + size // 2, size // 2, counts) # 오른쪽 아래
-                return
+            if paper[i][j] != initial_color:
+                all_same = False
+                break
+        if not all_same:
+            break
     
-    if color == 0:
-        counts[0] += 1
+    # 모두 동일한 색이라면 해당 색의 종이 개수 증가
+    if all_same:
+        if initial_color == 0:
+            white_count += 1
+        else:
+            blue_count += 1
+    # 색이 섞인 경우 : 4등분하여 재귀 호출
     else:
-        counts[1] += 1
+        half_size = size // 2
+        calculate_counts(x, y, half_size) # 1사분면
+        calculate_counts(x, y + half_size, half_size) # 2사분면
+        calculate_counts(x + half_size, y, half_size) # 3사분면
+        calculate_counts(x + half_size, y + half_size, half_size) # 4사분면
 
-counts = [0, 0]  # counts[0]: 0의 개수, counts[1]: 1의 개수
-recursive_count(0, 0, N, counts)
-print(f"{counts[0]}\n{counts[1]}")
+# 함수 실행 & 결과 출력
+calculate_counts(0, 0, N)
+sys.stdout.write(f"{white_count}\n{blue_count}\n")
