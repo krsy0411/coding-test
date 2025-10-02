a2fd0cad20fb44d03270ed182fc48c791f3eceec
ThisTimeNull <90031820+krsy0411@users.noreply.github.com>
2025-10-02 12:53:11 +0900

    [Gold V] Title: Z, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.py
+++ 백준/Gold/1074. Z/Z.py
@@ -1,24 +1,36 @@
 import sys
+sys.setrecursionlimit(10**6)
+
 input = sys.stdin.readline
-N,r,c = list(map(int, input().strip().split()))
 
-def z_order(n,r,c):
-    if n == 0:
-        return 0
+N, r, c = map(int, input().strip().split())
+result = 0 # 결과 : 몇 번째로 방문했는지
+
+def z(x,y,n):
+    global result
     
-    if r < 2**(n-1) and c < 2**(n-1):
-        # 1사분면
-        return z_order(n-1, r, c) + 0
-    elif r < 2**(n-1) and c >= 2**(n-1):
-        # 2사분면
-        return z_order(n-1, r, c - 2**(n-1)) + 2**(n-1) * 2**(n-1)
-    elif r >= 2**(n-1) and c < 2**(n-1):
-        # 3사분면
-        return z_order(n-1, r - 2**(n-1), c) + 2**(n-1) * 2**(n-1) * 2
+    size = (2**n)
+    if(size == 1): return # 기저 조건
+    
+    # 재귀 호출하면서 사이즈 줄여나가고 결과값 갱신
+    new_size = (2**(n-1))
+    if((r < (x + new_size)) and (c < (y + new_size))):
+        # r,c가 1사분면에 속한 경우
+        result += 0
+        z(x, y, n-1)
+    elif((r < (x + new_size)) and (c >= (y + new_size))):
+        # r,c가 2사분면에 속한 경우
+        result += (new_size**2)
+        z(x, y + new_size, n-1)
+    elif((r >= (x + new_size)) and (c < (y + new_size))):
+        # r,c가 3사분면에 속한 경우
+        result += ((new_size**2) * 2)
+        z(x + new_size, y, n-1)
     else:
-        # 4사분면
-        return z_order(n-1, r - 2**(n-1), c - 2**(n-1)) + 2**(n-1) * 2**(n-1) * 3
+        # r,c가 4사분면에 속한 경우
+        result += ((new_size**2) * 3)
+        z(x + new_size, y + new_size, n-1)
 
-# r행c열의 값
-result = z_order(N, r, c)
-print(result)
+# 함수 실행 & 결과 출력
+z(0,0,N)
+sys.stdout.write(str(result))
