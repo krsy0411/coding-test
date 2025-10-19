04b8c19cbdfb2aa2b9428f059bcf5066829e078c
ThisTimeNull <90031820+krsy0411@users.noreply.github.com>
2025-10-19 22:44:27 +0900

    [level 2] Title: 괄호 회전하기, Time: 64.25 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,26 +1,38 @@
 from collections import deque
 
+def check(current_list, stack):
+    result = True
+    
+    bracket_map = {')': '(', ']': '[', '}': '{'}
+    for char in current_list:
+        if char in '({[':
+                stack.append(char)
+        elif char in ')}]':
+            if (not stack) or (stack.pop() != bracket_map[char]):
+                result = False
+                break
+    
+    return result
+
 def solution(s):
     answer = 0 # 올바른 괄호 문자열이 안 만들어지면 0 유지
     rotate_num = len(s) # 회전 횟수
     queue = deque(list(s))
     
-    bracket_map = {')': '(', ']': '[', '}': '{'}
+    bracket_map = {
+        ')': '(', 
+        ']': '[', 
+        '}': '{'
+    }
     for _ in range(rotate_num):
         # 큐 회전
         queue.append(queue.popleft())
         
-        # 스택 이용해서 올바른 짝을 이루는지 체크2
+        # 스택 이용해서 올바른 짝을 이루는지 체크
         stack = []
         current_list = list(queue)
-        is_okay = True
-        for char in current_list:
-            if char in '({[':
-                stack.append(char)
-            elif char in ')}]':
-                if (not stack) or (stack.pop() != bracket_map[char]):
-                    is_okay = False
-                    break
+        is_okay = check(current_list, stack)
+
         # 결과 확인 : 짝을 이루면서 스택이 비어있다면 카운트 증가
         if is_okay and (not stack):
             answer += 1
