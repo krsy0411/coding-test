e3432e2170e80e8f17e6d59dce7dc470630c53e6
ThisTimeNull <90031820+krsy0411@users.noreply.github.com>
2026-01-29 17:28:58 +0900

    [Silver II] Title: 최대 힙, Time: 104 ms, Memory: 41172 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -1,19 +1,21 @@
-# 백준 11279 : 최대 힙
 import sys
 import heapq
+
 input = sys.stdin.readline
 
-N = int(input().strip())
-pq = []
-result = [] # 결과출력용 값 모음 배열
+n = int(input().strip())
+max_heap = []
+result = []
 
-for _ in range(N):
-    x = int(input().strip()) # 연산에 대한 정보를 나타내는 정수x
+for _ in range(n):
+    input_num = int(input().strip())
     
-    if x == 0:
-        num = -heapq.heappop(pq) if pq else 0 # 값이 있다면 힙에서 값 꺼낼때 반대 부호 적용(최대힙처럼 동작하도록 했기 때문에)
-        result.append(num)
+    if input_num == 0:
+        if max_heap:
+            result.append(-heapq.heappop(max_heap))
+        else:
+            result.append(0)
     else:
-        heapq.heappush(pq, -x) # 최대 힙처럼 동작하도록 음수로
-    
-sys.stdout.write('\n'.join(map(str, result)) + '\n')
+        heapq.heappush(max_heap, -input_num)
+
+print('\n'.join(map(str, result)))
